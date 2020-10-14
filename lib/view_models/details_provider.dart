4dab3203c732fc5917ce4d75df8c8e4b7a39ab0d
Zaki Mubarok <kakzaki@gmail.com>
2020-10-14 15:03:28 +0700

    init

--- lib/view_models/details_provider.dart
+++ lib/view_models/details_provider.dart
@@ -95,11 +95,7 @@ class DetailsProvider extends ChangeNotifier {
   }
 
   Future downloadFile(BuildContext context, String url, String filename) async {
-    PermissionStatus permission = await PermissionHandler()
-        .checkPermissionStatus(PermissionGroup.storage);
-
-    if (permission != PermissionStatus.granted) {
-      await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+    if (await Permission.storage.request().isGranted) {
       startDownload(context, url, filename);
     } else {
       startDownload(context, url, filename);
